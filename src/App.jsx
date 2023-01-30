a59099815c0e7eea0dadee38a772dc8e3ba56043
njaina <hei.njaina.2@gmail.com>
2023-01-30 09:07:15 +0300

    feat : update apps

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/**class Implemenet extends React.Component(){
+    constructor(props){
+        super(props);
+        this.props={value: 0}
+    }
+
+    render(){
+        return(
+            <>
+            <button onClick={()=>this.setState({value:this.state.value+1})} >+</button>
+            <div>{this.state.value}</div>
+            </>
+        )
+    }
+}*/
