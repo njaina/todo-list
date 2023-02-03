8fa78d67f5b6320f2c75ed1696f98065e4f65aa9
njaina <hei.njaina.2@gmail.com>
2023-02-03 05:40:27 +0300

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,57 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css"
-
+import DoneList from "./Components/DoneList";
+import TodoList from "./Components/TodoList";
 function App() {
-  const [value,setValue]=useState("");
-  const [InputValues, setInputValues] = useState([]);
-  const [done, setDone] = useState([]);
-  
-  
-
-  const handleInputChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      setInputValues([...InputValues,value]);
-      setValue('');
-    }
-  };
-  
-  const handleCheck = (index) => {
-    const doneItem = InputValues[index];
-    const newTodos = InputValues.filter((_, i) => i !== index);
-    setDone([...done, doneItem]);
-    setInputValues(newTodos);
-  };
-  
-  return (
-    <div className="main"> 
-    <div className="container">
-    <div className="todo">
-      <div className="title">Todo</div>
-      <div>
-        <input
-          className="input"
-          type="text"
-          placeholder="New to do"
-          value={value}
-          onChange={handleInputChange}
-          onKeyPress={handleKeyPress}
-        />
-        <div>
-      
-        {InputValues.map((todo, index) => (
-          <div className="response" >
-            <div className="element" key={todo} >{todo}</div>
-            <div className="check">
-            <input  type="checkbox" onClick={() => handleCheck(index)} /></div>
-            </div>
-            ))}
-        </div>
-      </div>
-    </div>
-    </div>
-     
-    <div className="container">
-    <div className="done">
-    <div className="title" >Done</div>
-    <div>
-      {done.map((todo) => (
+     const [value, setValue] = React.useState("");
+     const [InputValues, setInputValues] = React.useState([]);
+     const [done, setDone] = React.useState([]);
+   
+     const handleInputChange = (event) => {
+       setValue(event.target.value);
+     };
+   
+     const handleKeyPress = (event) => {
+       if (event.key === "Enter") {
+         setInputValues([...InputValues, value]);
+         setValue("");
+       }
+     };
+   
+     const handleCheck = (index) => {
+       const doneItem = InputValues[index];
+       const newTodos = InputValues.filter((elt, i) => i !== index);
+       setDone([...done, doneItem]);
+       setInputValues(newTodos);
+     };
+   
+     return (
+       <div className="main">
+         <div className="container">
+           <div className="todo">
+             <div className="title">Todo</div>
+             <div>
+               <input
+                 className="input"
+                 type="text"
+                 placeholder="New to do"
+                 value={value}
+                 onChange={handleInputChange}
+                 onKeyPress={handleKeyPress}
+               />
+               
+               <TodoList InputValues={InputValues} handleCheck={handleCheck} />
+             </div>
+           </div>
+         </div>
+   
+         <div className="container">
+           <div className="done">
+             <div className="title">Done</div>
+             <DoneList done={done} />
+           </div>
         
-        <div className="response">
-          <div className="done-elt"  key={todo}>{todo}</div>
-        </div>
-      ))}
-    </div>
-    </div>
-    </div>
+   </div>
     </div>
   );
 }
